ee99250fe6418632021bc3c87acd360e58bffcb7
Vecna <vvecna@uwaterloo.ca>
2025-04-04 15:39:38 -0400

    Too many issues with setup script, just remove it

--- 2025 Spring/setup-debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt install curl git python-is-python3 python3 python3-dev python3-pip python3-venv && \
-python3 -m venv . && source bin/activate && \
-pip install torch --index-url https://download.pytorch.org/whl/cpu && \
-pip install -r requirements.txt && \
-mkdir -p modules && git clone https://github.com/gianlucatruda/TableDiffusion.git modules/TableDiffusion && \
-curl -LO "https://physionet.org/files/eicu-crd-demo/2.0.1/patient.csv.gz?download" && gunzip patient.csv.gz && \
-mv patient.csv patient.csv.orig && sed '/,,/d' patient.csv.orig > patient.csv
